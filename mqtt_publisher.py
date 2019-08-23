7b9efe6b28cd0f6ca7c438c688e425fc8d5576e8
wissemnano <wissem.mansouri.ing@gmail.com>
2019-08-23 11:17:53 +0200

    subsb

--- mqtt_publisher.py
+++ mqtt_publisher.py
@@ -1,6 +1,6 @@
 import paho.mqtt.publish as publish
  
-MQTT_SERVER = "192.168.1.25"
+MQTT_SERVER = "192.168.1.69"
 MQTT_PATH = "test_channel"
  
 publish.single(MQTT_PATH, "Hello World!", hostname=MQTT_SERVER)
